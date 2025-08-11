a595e811b84f80bd90c2d42212158beecd991064
OngarjPC <ongarj2314@gmail.com>
2025-08-11 23:03:50 +0700

    เหลือ ข้อ5 ยังทำไม่ได้

--- week01/practice_hightorder_func.js
+++ week01/practice_hightorder_func.js
@@ -1,7 +1,7 @@
 // Map
 // Square the value of every element in the array
 let Square = [1, 2, 3, 4, 5].map(value => value * value);
-console.log(Square);
+// console.log(Square);
 /*
 Map Filter Reduce Foreach
 ใช้ filter
@@ -25,57 +25,80 @@ const products = [
   { name: "Tablet", price: 12000, stock: 8 },
 ];
 
-let stock = products.filter(data => data.stock > 0);
-console.log(stock)
+// let stock = products.filter(data => data.stock > 0);
+// console.log(stock)
 
-let tax = products.map(data => ({name: data.name , price_tax: data.price * 1.07 }))
-console.log(tax)
+// let tax = products.map(data => ({ name: data.name, price_tax: data.price * 1.07 }))
+// console.log(tax)
 
-let sum_stock = products.reduce((acc,value) => acc + value.price*value.stock,0);
-console.log(sum_stock)
+// let sum_stock = products.reduce((acc, value) => (value.price * value.stock) + acc, 0);
+// console.log(sum_stock)
 
-products.forEach(data => console.log(`สินค้า ${data.name} มีราคา ${data.price} บาท (รวม VAT)`))
+// products.forEach(data => console.log(`สินค้า ${data.name} มีราคา ${data.price} บาท (รวม VAT)`))
 
 // โจทย์ผสม Map Filter Reduce Foreach
 const orders = [
-  { id: 1, customer: "Alice", items: [
+  {
+    id: 1, customer: "Alice", items: [
       { product: "Laptop", price: 32000, quantity: 1 },
       { product: "Mouse", price: 800, quantity: 2 }
     ]
   },
-  { id: 2, customer: "Bob", items: [
+  {
+    id: 2, customer: "Bob", items: [
       { product: "Tablet", price: 12000, quantity: 1 },
       { product: "Keyboard", price: 1500, quantity: 1 }
     ]
   },
-  { id: 3, customer: "Charlie", items: [
+  {
+    id: 3, customer: "Charlie", items: [
       { product: "Monitor", price: 7200, quantity: 2 },
       { product: "Mouse", price: 800, quantity: 1 }
     ]
   },
-  { id: 4, customer: "Alice", items: [
+  {
+    id: 4, customer: "Alice", items: [
       { product: "Keyboard", price: 1500, quantity: 3 }
     ]
   },
 ];
 
 /*
-ใช้ map + reduce
+ข้อที่1 ใช้ map + reduce
 แปลง orders ให้เป็น array ใหม่ที่มีแต่ { customer, total }
-
 total คือราคารวมของ items ในแต่ละ order (price × quantity)
 
-ใช้ filter
+ข้อที่2 ใช้ filter
 เลือกเฉพาะ order ที่ total มากกว่า 15,000
 
-ใช้ reduce
+ข้อที่3 ใช้ reduce
 หาผลรวมของยอดขายจากทุก order ที่ผ่านการ filter
 
-ใช้ forEach
+ข้อที่4 ใช้ forEach
 แสดงผลลัพธ์ของ order ที่ผ่านการ filter แบบนี้:
-
 ลูกค้า Alice มียอดสั่งซื้อ 33,600 บาท
-ขั้นเสริม (ยากขึ้น)
+
+ข้อที่5 ขั้นเสริม (ยากขึ้น)
 รวมยอดขายของแต่ละลูกค้า (Alice อาจมีหลาย order ให้รวมกัน) แล้วแสดงเรียงจากยอดขายมาก → น้อย
 
 */
+let total_order = orders.map(order => {
+  let total = order.items.reduce((acc, value) => {
+     return (value.price * value.quantity) + acc 
+    },0
+  )
+  return { customer: order.customer,total:total}
+});
+console.log(total_order)// ข้อที่ 1
+
+let filter_total = total_order.filter(order => order.total > 15000);
+console.log(filter_total)//ข้อที่ 2 
+
+let sumfilter_order = filter_total.reduce((acc,value) => value.total + acc,0)
+console.log(sumfilter_order)//ข้อที่ 3
+
+filter_total.forEach(element => {
+  console.log(`ลูกค้า ${element.customer} มียอดสั่งซื้อ ${element.total} บาท`)
+})//ข้อที่ 4
+
+

56fed198c69f9a18b7a8c1700295acb972c8f534
LT_Ongarj <ongarj2314@gmail.com>
2025-09-05 23:16:45 +0700

    add Boss Practice Final Week 5

--- week05/Test_JsModules/commonJSTest.js
+++
@@ -1,3 +0,0 @@
-const {echo,getLength,MAXStudent} = require('./utility.js');
-console.log(echo(MAXStudent))
-console.log(getLength("do something...."))
--- week05/esModule/esModuleTest.js
+++
@@ -1,5 +0,0 @@
-import strLength, { echo as repeat, VAT } from '../Test_JsModules/utility'
-
-console.log(repeat("No one perfect"))
-console.log(VAT)
-console.log(strLength("Frontend"))
